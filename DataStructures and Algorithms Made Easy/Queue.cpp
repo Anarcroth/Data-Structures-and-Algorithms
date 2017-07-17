d4097c8dcb5d78c9963f8cf3b787c39d681dc2b8
Martin Nestorov <mnestorov@protonmail.com>
2017-07-17 20:50:58 +0300

    re-implemented the queue file.

--- DataStructures and Algorithms Made Easy/Queue.cpp
+++ DataStructures and Algorithms Made Easy/Queue.cpp
@@ -2,58 +2,96 @@
 
 struct node
 {
-	node *next;
 	int key;
+	node *next;
 };
 
-class Queue
+struct queue
 {
-public:
-	Queue();
-	
-	~Queue();
-	
-	void insert_key(int key);
-	node search_key(int key);
-	void create_queue(int key);
-
-private:
-	node *en_queue = NULL;
-	node *de_queue = NULL;
-
-	void insert_key(node* next, int key);
-	node search_key(node* next, int key);
+	node *front;
+	node *end;
 };
 
-Queue::Queue()
+queue *create_queue()
 {
+	queue *q;
+	node *temp;
+
+	q = (queue*)malloc(sizeof(queue));
+
+	if (!q)
+		return NULL;
+
+	temp = (node*)malloc(sizeof(node));
+
+	q->front = q->end = NULL;
+
+	return q;
 }
 
-Queue::~Queue()
+int is_queue_empty(queue *q)
 {
+	return q->end == NULL;
 }
 
-void Queue::create_queue(int key)
+void en_queue(queue *q, int key)
 {
-	if (en_queue == NULL)
+	node *new_node;
+
+	new_node = (node*)malloc(sizeof(node));
+
+	if (!new_node)
+		NULL;
+
+	new_node->key = key;
+	new_node->next = NULL;
+	
+	if (!q->end) q->end->next = new_node;
+
+	q->end = new node;
+
+	if (q->front == NULL)
+		q->front = q->end;
+}
+
+int de_queue(queue *q)
+{
+	int key = 0;
+	
+	node* temp;
+	
+	if (is_queue_empty(q))
 	{
-		en_queue = new node;
-		en_queue->key = key;
-		en_queue->next = NULL;
+		printf("%s", "Queue is empty");
+		return 0;
 	}
+	else
+	{
+		temp = q->front;
+		key = q->front->key;
+		q->front == q->front->next;
+		free(temp);
+	}
+
+	return key;
 }
 
-void Queue::insert_key(int key)
+void delete_queue(queue *q)
 {
 	node *temp;
 
-	if (en_queue != NULL)
+	while (q)
 	{
-		//while ()
+		temp = (node*)q;
+		q = (queue*)q->front;
+		free(temp);
 	}
+
+	free(q);
 }
 
 int main()
 {
 
+	return 0;
 }
