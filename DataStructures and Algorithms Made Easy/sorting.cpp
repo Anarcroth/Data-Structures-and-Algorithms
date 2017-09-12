738d59af0b22ddc90dd10d9a65308a858b560281
Martin <mnestorov@documaster.no>
2017-09-12 20:19:31 +0300

    Added merge sort implementation

--- DataStructures and Algorithms Made Easy/sorting.cpp
+++ DataStructures and Algorithms Made Easy/sorting.cpp
@@ -1,6 +1,11 @@
-//
-// Created by mdn on 09.09.17.
-//
+/**
+ * COS: FDS
+ * sorting.cpp
+ * Purpose: Sort a vector of integers using multiple sorting algorithms
+ *
+ * @author: Martin Nestorov
+ * @version: 0.1.0 12/09/2017
+ */
 
 #include <cstdlib>
 #include <random>
@@ -29,14 +34,6 @@ void sorting<T>::selection_sort(std::vector<T> &array, T size)
                 array[i] = temp;
             }
         }
-    }
-
-
-    std::cout << "\n\nSelection Sort: \n";
-
-    for (auto it = array.begin(); it != array.end(); it++)
-    {
-        std::cout << *it << " ";
     }
 }
 
@@ -106,13 +103,6 @@ void sorting<T>::insertion_sort(std::vector<T> &array, T size)
 
         array[j] = x;
     }
-
-    std::cout << "\n\nInsertion Sort: \n";
-
-    for (auto it = array.begin(); it != array.end(); it++)
-    {
-        std::cout << *it << " ";
-    }
 }
 
 template<class T>
@@ -129,16 +119,14 @@ void sorting<T>::quick_sort(std::vector<T> &array, int low, int high)
 template<class T>
 int sorting<T>::partition(std::vector<T> &array, int low, int high)
 {
-    int pivot = array[high];    // pivot
-    int i = (low - 1);  // Index of smaller element
+    int pivot = array[high];
+    int i = (low - 1);
 
     for (int j = low; j <= high- 1; j++)
     {
-        // If current element is smaller than or
-        // equal to pivot
         if (array[j] <= pivot)
         {
-            i++;    // increment index of smaller element
+            i++;
             swap(&array[i], &array[j]);
         }
     }
@@ -157,6 +145,70 @@ void sorting<T>::swap(T *low, T *high)
 }
 
 template<class T>
+void sorting<T>::merge_sort(std::vector<T> &array, int left, int right)
+{
+    if (left < right)
+    {
+        int middle = left + (right - left) / 2;
+
+        merge_sort(array, left, middle);
+        merge_sort(array, middle + 1, right);
+
+        merge(array, left, middle, right);
+    }
+}
+
+template<class T>
+void sorting<T>::merge(std::vector<T> &array, int left, int middle, int right)
+{
+    int i, j, k;
+    int n1 = middle - left + 1;
+    int n2 =  right - middle;
+
+    std::vector<T> L(n1), R(n2);
+
+    for (i = 0; i < n1; i++)
+        L[i] = array[left + i];
+
+    for (j = 0; j < n2; j++)
+        R[j] = array[middle + 1+ j];
+
+    i = 0;
+    j = 0;
+    k = left;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            array[k] = L[i];
+            i++;
+        }
+        else
+        {
+            array[k] = R[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while (i < n1)
+    {
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+template<class T>
 std::vector<T> sorting<T>::generate_rand_array(std::vector<T> &array, int size)
 {
     srand(time(nullptr));
@@ -167,6 +219,15 @@ std::vector<T> sorting<T>::generate_rand_array(std::vector<T> &array, int size)
     }
 
     return array;
+}
+
+template<class T>
+void sorting<T>::display(std::vector<T> &array)
+{
+    for (int &it : array)
+    {
+        std::cout << it << " ";
+    }
 }
 
 int main()
@@ -188,24 +249,31 @@ int main()
 
     sorting<int> sort_algthms;
 
-    std::vector<int> array, array2, array3, array4;
+    std::vector<int> array, array2, array3, array4, array5;
 
     array = sort_algthms.generate_rand_array(array, size);
     array2 = sort_algthms.generate_rand_array(array2, size);
     array3 = sort_algthms.generate_rand_array(array3, size);
     array4 = sort_algthms.generate_rand_array(array4, size);
+    array5 = sort_algthms.generate_rand_array(array5, size);
 
     sort_algthms.bubble_sort(array, size);
+
+    std::cout << "\n\nInsertion Sort: \n";
     sort_algthms.insertion_sort(array2, size);
+    sort_algthms.display(array2);
+
+    std::cout << "\n\nSelection Sort: \n";
     sort_algthms.selection_sort(array3, size);
-    sort_algthms.quick_sort(array4, 0, size);
+    sort_algthms.display(array3);
 
     std::cout << "\n\nQuick Sort: \n";
+    sort_algthms.quick_sort(array4, 0, size);
+    sort_algthms.display(array4);
 
-    for (int &it : array4)
-    {
-        std::cout << it << " ";
-    }
+    std::cout << "\n\nMerge Sort: \n";
+    sort_algthms.merge_sort(array5, 0, size - 1);
+    sort_algthms.display(array5);
 
     return 0;
 }
--- DataStructures and Algorithms Made Easy/sorting.h
+++ DataStructures and Algorithms Made Easy/sorting.h
@@ -12,14 +12,20 @@ private:
 public:
     std::vector<T> generate_rand_array(std::vector<T> &array, int size);
 
-    void quick_sort(std::vector<T> &array, int low, int high);
     void selection_sort(std::vector<T> &array, T size);
+
     void insertion_sort(std::vector<T> &array, T size);
+
     void bubble_sort(std::vector<T> &array, T size);
-    void swap(T *low, T *high);
-    void merge_sort(T[], T);
 
     int partition(std::vector<T> &array, int low, int high);
+    void quick_sort(std::vector<T> &array, int low, int high);
+    void swap(T *low, T *high);
+
+    void merge(std::vector<T> &array, int left, int middle, int right);
+    void merge_sort(std::vector<T> &array, int left, int right);
+
+    void display(std::vector<T> &array);
 };
 
 
