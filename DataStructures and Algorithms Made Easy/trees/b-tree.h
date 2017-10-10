9d9b54e92a024686fcb1cd25455a6a3f61b94e93
Martin Nestorov <mnestorov@protonmail.com>
2017-10-10 17:37:55 +0300

    Added B-Tree implementation.

--- DataStructures and Algorithms Made Easy/trees/b-tree.h
+++ DataStructures and Algorithms Made Easy/trees/b-tree.h
@@ -8,13 +8,24 @@
 // A BTree node
 class node
 {
+private:
     int *keys;  // An array of keys
     int t;      // Minimum degree (defines the range for number of keys)
     node **C; // An array of child pointers
     int n;     // Current number of keys
     bool leaf; // Is true when node is leaf. Otherwise false
+
 public:
-    node(int _t, bool _leaf);   // Constructor
+    // A utility function to insert a new key in the subtree rooted with
+    // this node. The assumption is, the node must be non-full when this
+    // function is called
+    void insertNonFull(int k);
+
+    // A utility function to split the child y of this node. i is index of y in
+    // child array C[].  The Child y must be full when this function is called
+    void splitChild(int i, node *y);
+
+    node(int t, bool leaf);   // Constructor
 
     // A function to traverse all nodes in a subtree rooted with this node
     void traverse();
@@ -34,7 +45,7 @@ class BTree
     int t;  // Minimum degree
 public:
     // Constructor (Initializes tree as empty)
-    BTree(int _t)
+    explicit BTree(int _t)
     {  root = nullptr;  t = _t; }
 
     // function to traverse the tree
@@ -44,6 +55,9 @@ public:
     // function to search a key in this tree
     node* search(int k)
     {  return (root == nullptr)? nullptr : root->search(k); }
+
+    // The main function that inserts a new key in this B-Tree
+    void insert(int k);
 };
 
 #endif //DATASTRUCTURES_AND_ALGORITHMS_MADE_EASY_B_TREE_H
